d8cf09651c01c43df71088fbebe065e590b46ba2
st24hour <st24hour@kaist.ac.kr>
2023-12-15 04:55:13 +0000

    Initial commit

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 python train.py \
-            --num_gpus 16 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py \
+            --num_gpus 4 \
             --seed 1 \
             --lr 0.00025 \
             --clip_grad 1 \
