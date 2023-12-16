d31b7158fea6a9cfb765d6ea17860ca33850f421
st24hour <st24hour@kaist.ac.kr>
2023-12-16 17:54:47 +0000

    initial commit

--- docker1.sh
+++
@@ -1,5 +0,0 @@
-docker run --gpus all --shm-size=256g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm --pid=host -v $(pwd):$(pwd) -w $(pwd) \
-	-v /etc/localtime:/etc/localtime:ro -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro --name js_9 \
-	-v /shared:/shared/ st24hour/pytorch:1.12.1_SM \
-	sh train1.sh
-
